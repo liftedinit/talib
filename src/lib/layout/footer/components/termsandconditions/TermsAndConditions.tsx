f1dc5e6cbe99b477f5f561c7caff4f335201cbc2
Maximiliano Sorribas <mjsorribas@gmail.com>
2022-08-22 16:42:39 -0300

    feat: add style to termsAndCodition component

--- src/lib/layout/footer/components/termsandconditions/TermsAndConditions.tsx
+++ src/lib/layout/footer/components/termsandconditions/TermsAndConditions.tsx
@@ -1,16 +1,30 @@
 import { Box, Stack, Link } from "@chakra-ui/react";
 
+import { theme } from "lib/styles/customTheme";
+
 export default function TermsAndConditions() {
   return (
     <Stack align={{ md: "flex-end", sm: "center" }} spacing={6}>
       <Box fontSize="sm">
-        <Link href="https://theliftedinit.org/terms" ml={3}>
+        <Link
+          color={theme.colors.cream}
+          href="https://theliftedinit.org/terms"
+          ml={3}
+        >
           Terms
         </Link>
-        <Link href="https://theliftedinit.org/protocol" ml={3}>
+        <Link
+          color={theme.colors.cream}
+          href="https://theliftedinit.org/protocol"
+          ml={3}
+        >
           Protocol
         </Link>
-        <Link href="https://theliftedinit.org/cookies" ml={3}>
+        <Link
+          color={theme.colors.cream}
+          href="https://theliftedinit.org/cookies"
+          ml={3}
+        >
           Cookies
         </Link>
       </Box>

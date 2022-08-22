91776c8efbe66c9d6c523ebbbbda8327543df9e4
Maximiliano Sorribas <mjsorribas@gmail.com>
2022-08-22 16:56:00 -0300

    feat: update fontweight to 500  in TermsAndCoditions component

--- src/lib/layout/footer/components/termsandconditions/TermsAndConditions.tsx
+++ src/lib/layout/footer/components/termsandconditions/TermsAndConditions.tsx
@@ -5,7 +5,7 @@ import { theme } from "lib/styles/customTheme";
 export default function TermsAndConditions() {
   return (
     <Stack align={{ md: "flex-end", sm: "center" }} spacing={6}>
-      <Box fontSize="sm">
+      <Box fontSize="sm" fontWeight={500}>
         <Link
           color={theme.colors.cream}
           href="https://theliftedinit.org/terms"

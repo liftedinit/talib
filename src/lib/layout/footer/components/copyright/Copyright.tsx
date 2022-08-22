b8253ddde09e1e08b07588f080ad756b8e33ac9a
Maximiliano Sorribas <mjsorribas@gmail.com>
2022-08-22 16:43:48 -0300

    feat: add style to Copyright component

--- src/lib/layout/footer/components/copyright/Copyright.tsx
+++ src/lib/layout/footer/components/copyright/Copyright.tsx
@@ -4,7 +4,9 @@ export default function Copyright() {
   return (
     <Stack align={{ md: "flex-start", sm: "center" }} spacing={6}>
       <Box fontSize="sm">
-        <Text>© 2022 THE LIFTED INITIATIVE. All rights reserved</Text>
+        <Text color="Cream">
+          © 2022 THE LIFTED INITIATIVE. All rights reserved
+        </Text>
       </Box>
     </Stack>
   );

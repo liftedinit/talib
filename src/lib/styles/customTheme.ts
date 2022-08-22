165106438f7ee40490e5f4f1475fc582b805b205
Maximiliano Sorribas <mjsorribas@gmail.com>
2022-08-22 16:42:11 -0300

    feat: define custom brand style

--- src/lib/styles/customTheme.ts
+++ src/lib/styles/customTheme.ts
@@ -2,12 +2,66 @@ import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
   fonts: {
-    heading: "Plus Jakarta Sans, sans-serif",
-    body: "Plus Jakarta Sans, sans-serif",
+    heading: "`'Rubik', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'`",
+    body: "`'Rubik', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'`",
+  },
+  colors: {
+    white: "#ffffff",
+    black: "#000000",
+    green: "#8ddfd4",
+    brown: "#654D43",
+    cream: "#FAF0EA",
+  },
+  breakpoints: {
+    sm: "320px",
+    md: "768px",
+    lg: "960px",
+    xl: "1200px",
+  },
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        bg: "#faf0ea",
+        color: "#452e33",
+        fontSize: "16px",
+        fontWeight: "100",
+      },
+      // styles for the `a`
+      a: {
+        color: "#452e33",
+        _hover: {
+          textDecoration: "underline",
+          color: "#8ddfd4",
+        },
+      },
+    },
   },
   components: {
-    // Button: {
-    // }
+    Footer: {
+      bg: "#654D43",
+      color: "#FAF0EA",
+    },
+    SocialLinks: {
+      bg: "#8ddfd4",
+      color: "#654D43",
+    },
+    Sitemap: {
+      a: {
+        color: "#ffffff",
+      },
+    },
+    Logo: {
+      color: "#faf0ea",
+    },
+    Copyright: {
+      color: "#faf0ea",
+    },
+    TermsAndConditions: {
+      a: {
+        color: "#faf0ea",
+      },
+    },
   },
   config: {
     initialColorMode: "light",

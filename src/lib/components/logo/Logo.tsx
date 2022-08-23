098e67eea172149382816a8c8e3fd84bd80d74ac
Maximiliano Sorribas <mjsorribas@gmail.com>
2022-08-23 13:19:48 -0300

    feat: update Logo component, add param to set boxSize and fontSize

--- src/lib/components/logo/Logo.tsx
+++ src/lib/components/logo/Logo.tsx
@@ -2,27 +2,30 @@ import { Image, Box, Text } from "@chakra-ui/react";
 
 import { theme } from "lib/styles/customTheme";
 
-const LOGOSVG = () => {
+interface Props {
+  boxSize: string;
+  fontSize: string;
+}
+
+const Logo = ({ boxSize, fontSize }: Props) => {
   return (
     <Box display="flex" alignItems="center">
       <Image
-        boxSize="100px"
+        boxSize={boxSize}
         objectFit="cover"
         src="/assets/lifted.svg"
         title="The Lifted initiative"
       />
-      <Text color={theme.colors.cream} fontSize="1.168rem" fontWeight={300}>
+      <Text color={theme.colors.cream} fontSize={fontSize} fontWeight={300}>
         THE
       </Text>
-      <Text color={theme.colors.cream} fontSize="1.168rem" fontWeight={700}>
+      <Text color={theme.colors.cream} fontSize={fontSize} fontWeight={700}>
         LIFTED
       </Text>
-      <Text color={theme.colors.cream} fontSize="1.168rem" fontWeight={300}>
+      <Text color={theme.colors.cream} fontSize={fontSize} fontWeight={300}>
         INITIATIVE
       </Text>
     </Box>
   );
 };
-export default function Logo() {
-  return <LOGOSVG />;
-}
+export default Logo;

255082df26512f166e90ed4d21282df856323b38
Maximiliano Sorribas <mjsorribas@gmail.com>
2022-08-30 12:43:50 -0300

    feat: #8 update homepage,add BlockList component

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,7 +1,13 @@
 import { Grid } from "@chakra-ui/react";
 
+import BlockList from "lib/components/blocklist/BlockList";
+
 const Home = () => {
-  return <Grid gap={4} />;
+  return (
+    <Grid gap={4}>
+      <BlockList />
+    </Grid>
+  );
 };
 
 export default Home;

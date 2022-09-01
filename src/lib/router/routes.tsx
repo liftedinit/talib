8143ae98f18aa0ac9efb9de01756ed8638ba13c6
Maximiliano José Sorribas <mjsorribas@gmail.com>
2022-09-01 12:19:13 -0300

    feat: add nodes index page to routes. close #13

--- src/lib/router/routes.tsx
+++ src/lib/router/routes.tsx
@@ -1,11 +1,16 @@
 import type { PathRouteProps } from "react-router-dom";
 
 import Home from "lib/pages/home";
+import Nodes from "lib/pages/nodes";
 
 export const routes: Array<PathRouteProps> = [
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/nodes",
+    element: <Nodes />,
   },
 ];
 

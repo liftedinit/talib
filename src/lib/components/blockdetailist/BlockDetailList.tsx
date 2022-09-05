183d56de39fcb4478b2301fd5d57ef504f04fa5a
Armando Herrera <armandoher01@gmail.com>
2022-09-05 16:22:31 -0300

    fix: fix type to table

--- src/lib/components/blockdetailist/BlockDetailList.tsx
+++ src/lib/components/blockdetailist/BlockDetailList.tsx
@@ -10,8 +10,8 @@ type BlockDetail = {
   id: number;
   block_hash: string;
   time: string;
-  from: number;
-  to: number;
+  from: string;
+  to: string;
   type: string;
 };
 
@@ -21,64 +21,64 @@ const blocks: BlockDetail[] = [
     id: 1,
     block_hash: "457676765",
     time: "2hs 36min ago",
-    to: 9582305095533,
-    from: 5435364646666,
+    to: "9582305095533",
+    from: "5435364646666",
     type: "Buy",
   },
   {
     id: 2,
     block_hash: "832785237",
-    time: "4hs 16mins ago",
-    from: 9588675095533,
-    to: 5435364646666,
+    time: "4hs 15mins ago",
+    from: "9588675095533",
+    to: "5435364646666",
     type: "Sell",
   },
   {
     id: 3,
     block_hash: "756465466",
     time: "4hs 54mins ago",
-    to: 9588655095533,
-    from: 4356664646666,
+    to: "9588655095533",
+    from: "4356664646666",
     type: "Sign",
   },
   {
     id: 4,
     block_hash: "5465757566",
     time: "6hs 46mins ago",
-    to: 9582305095533,
-    from: 54353649877886,
+    to: "9582305095533",
+    from: "54353649877886",
     type: "Execute",
   },
   {
     id: 5,
     block_hash: "832785237",
     time: "4hs 16mins ago",
-    from: 9588675095533,
-    to: 5435364646666,
+    from: "9588675095533",
+    to: "5435364646666",
     type: "Buy",
   },
   {
     id: 6,
     block_hash: "756465466",
     time: "4hs 54mins ago",
-    to: 9588655095533,
-    from: 4356664646666,
+    to: "9588655095533",
+    from: "4356664646666",
     type: "Sign",
   },
   {
     id: 7,
     block_hash: "5465757566",
     time: "6hs 46mins ago",
-    to: 9582305095533,
-    from: 54353649877886,
+    to: "9582305095533",
+    from: "54353649877886",
     type: "Execute",
   },
   {
     id: 8,
     block_hash: "832785237",
     time: "4hs 16mins ago",
-    from: 9588675095533,
-    to: 5435364646666,
+    from: "9588675095533",
+    to: "5435364646666",
     type: "Buy",
   },
 ];

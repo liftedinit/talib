fe547b08a349cba6977a9253a3bd309ced289505
Armando Herrera <armandoher01@gmail.com>
2022-09-06 14:15:36 -0300

    fix: fix on table

--- src/lib/components/customtable/components/Table.tsx
+++ src/lib/components/customtable/components/Table.tsx
@@ -66,7 +66,7 @@ export function Table({
   }
 
   return (
-    <Box w={{ sm: "23em", md: "54em", lg: "82em" }} maxW="full">
+    <Box w={{ sm: "23em", md: "54em", lg: "82em" }} overflow="hidden">
       <Box px="0" borderRadius="8" w="full" h="auto">
         <ChakraTable {...getTableProps()}>
           <Thead>

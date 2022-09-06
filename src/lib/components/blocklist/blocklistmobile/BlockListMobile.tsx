0d36bd2a6b29544359be4b7f486e8162babe8a01
Maximiliano José Sorribas <mjsorribas@gmail.com>
2022-09-06 14:10:25 -0300

    feat: add BlockListMobile component

---
+++ src/lib/components/blocklist/blocklistmobile/BlockListMobile.tsx
@@ -0,0 +1,102 @@
+// Recommended for icons
+import React from "react";
+import { Box, Stack, Text, Table, Tr, Td } from "@chakra-ui/react";
+
+import { usePagination } from "lib/components/customtable/hooks/usePagination";
+
+import { theme } from "lib/styles/customTheme";
+
+import { NoContent } from "lib/components/customtable/components/NoContent";
+import type { NoContentProps } from "lib/components/customtable/components/NoContent";
+import { Pagination } from "lib/components/customtable/components/Pagination";
+
+type EmptyMessage = Partial<NoContentProps>;
+
+type Block = {
+  id: number;
+  block_hash: string;
+  time: string;
+  num_of_txs: number;
+  data_size: number;
+  nodes: number;
+};
+
+interface Props {
+  blocks: Block[];
+}
+
+const BlockListMobile = ({ blocks }: Props) => {
+  // Control current Page
+  const [page, setPage] = React.useState(1);
+  const totalRegisters = blocks.length;
+
+  const pagination = usePagination({
+    totalRegisters,
+    page,
+    items: blocks,
+  });
+
+  return (
+    <Box>
+      {blocks.map((block, index) => (
+        <Stack id="block_{index}">
+          <Table
+            key={block.id}
+            variant="unstyled"
+            borderBottom="1px"
+            borderBottomColor="gray.200"
+          >
+            <Tr p="1rem">
+              <Td>
+                <Text color="#654D43" fontSize="12px" fontWeight={400}>
+                  BLOCK
+                </Text>
+              </Td>
+              <Td isNumeric>{block.block_hash}</Td>
+            </Tr>
+            <Tr p="1rem">
+              <Td>
+                <Text color="#654D43" fontSize="12px" fontWeight={400}>
+                  TIME
+                </Text>
+              </Td>
+              <Td>{block.time}</Td>
+            </Tr>
+            <Tr p="1rem">
+              <Td>
+                <Text color="#654D43" fontSize="12px" fontWeight={400}>
+                  NUMBER OF TXS
+                </Text>
+              </Td>
+              <Td isNumeric>{block.num_of_txs}</Td>
+            </Tr>
+            <Tr p="1rem">
+              <Td>
+                <Text color="#654D43" fontSize="12px" fontWeight={400}>
+                  SIZE DATA
+                </Text>
+              </Td>
+              <Td isNumeric>{block.data_size}</Td>
+            </Tr>
+            <tr>
+              <Td>
+                <Text color="#654D43" fontSize="12px" fontWeight={400}>
+                  NODES
+                </Text>
+              </Td>
+              <Td isNumeric>{block.nodes}</Td>
+            </tr>
+          </Table>
+        </Stack>
+      ))}
+      <Box
+        width="full"
+        w={{ sm: "23em", md: "54em", lg: "82em" }}
+        maxW="full"
+        pt={0.5}
+        backgroundColor={theme.colors.cream}
+      ></Box>
+    </Box>
+  );
+};
+export default BlockListMobile;

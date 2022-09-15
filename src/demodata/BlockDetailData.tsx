9740932dadc8a03a178c794bbc164a6724eba3f6
Maximiliano José Sorribas <mjsorribas@gmail.com>
2022-09-15 19:21:49 -0300

    feat: add BlockDetailData demo file

---
+++ src/demodata/BlockDetailData.tsx
@@ -0,0 +1,71 @@
+// Example list of blocks, to replace it with many.js block list get method
+const BlockDetailData = () => {
+  return [
+    {
+      id: 1,
+      block_hash: "457676765",
+      time: "2hs 36min ago",
+      to: "9582305095533",
+      from: "5435364646666",
+      type: "Buy",
+    },
+    {
+      id: 2,
+      block_hash: "832785237",
+      time: "4hs 15mins ago",
+      from: "9588675095533",
+      to: "5435364646666",
+      type: "Sell",
+    },
+    {
+      id: 3,
+      block_hash: "756465466",
+      time: "4hs 54mins ago",
+      to: "9588655095533",
+      from: "4356664646666",
+      type: "Sign",
+    },
+    {
+      id: 4,
+      block_hash: "5465757566",
+      time: "6hs 46mins ago",
+      to: "9582305095533",
+      from: "54353649877886",
+      type: "Execute",
+    },
+    {
+      id: 5,
+      block_hash: "832785237",
+      time: "4hs 16mins ago",
+      from: "9588675095533",
+      to: "5435364646666",
+      type: "Buy",
+    },
+    {
+      id: 6,
+      block_hash: "756465466",
+      time: "4hs 54mins ago",
+      to: "9588655095533",
+      from: "4356664646666",
+      type: "Sign",
+    },
+    {
+      id: 7,
+      block_hash: "5465757566",
+      time: "6hs 46mins ago",
+      to: "9582305095533",
+      from: "54353649877886",
+      type: "Execute",
+    },
+    {
+      id: 8,
+      block_hash: "832785237",
+      time: "4hs 16mins ago",
+      from: "9588675095533",
+      to: "5435364646666",
+      type: "Buy",
+    },
+  ];
+};
+
+export default BlockDetailData();

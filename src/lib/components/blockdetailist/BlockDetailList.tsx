72afd24c490c042558172c708f9a9680f94923b2
Maximiliano José Sorribas <mjsorribas@gmail.com>
2022-09-15 19:21:04 -0300

    feat: update BlockDetrailList component, remove demo data and add import from demodata folder

--- src/lib/components/blockdetailist/BlockDetailList.tsx
+++ src/lib/components/blockdetailist/BlockDetailList.tsx
@@ -3,6 +3,7 @@ import { Box, Heading, Center } from "@chakra-ui/react";
 // Recommended for icons
 import React from "react";
 
+import BlockDetailData from "../../../demodata/BlockDetailData";
 import { Table } from "lib/components/customtable/CustomTable";
 import { theme } from "lib/styles/customTheme";
 
@@ -16,72 +17,7 @@ type BlockDetail = {
 };
 
 // Example list of blocks, to replace it with many.js block list get method
-const blocks: BlockDetail[] = [
-  {
-    id: 1,
-    block_hash: "457676765",
-    time: "2hs 36min ago",
-    to: "9582305095533",
-    from: "5435364646666",
-    type: "Buy",
-  },
-  {
-    id: 2,
-    block_hash: "832785237",
-    time: "4hs 15mins ago",
-    from: "9588675095533",
-    to: "5435364646666",
-    type: "Sell",
-  },
-  {
-    id: 3,
-    block_hash: "756465466",
-    time: "4hs 54mins ago",
-    to: "9588655095533",
-    from: "4356664646666",
-    type: "Sign",
-  },
-  {
-    id: 4,
-    block_hash: "5465757566",
-    time: "6hs 46mins ago",
-    to: "9582305095533",
-    from: "54353649877886",
-    type: "Execute",
-  },
-  {
-    id: 5,
-    block_hash: "832785237",
-    time: "4hs 16mins ago",
-    from: "9588675095533",
-    to: "5435364646666",
-    type: "Buy",
-  },
-  {
-    id: 6,
-    block_hash: "756465466",
-    time: "4hs 54mins ago",
-    to: "9588655095533",
-    from: "4356664646666",
-    type: "Sign",
-  },
-  {
-    id: 7,
-    block_hash: "5465757566",
-    time: "6hs 46mins ago",
-    to: "9582305095533",
-    from: "54353649877886",
-    type: "Execute",
-  },
-  {
-    id: 8,
-    block_hash: "832785237",
-    time: "4hs 16mins ago",
-    from: "9588675095533",
-    to: "5435364646666",
-    type: "Buy",
-  },
-];
+const blocks: BlockDetail[] = BlockDetailData;
 
 const BlockDetailList = () => {
   // Control current Page

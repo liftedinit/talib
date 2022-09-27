2ae103c7c97f7ac94db6dce6e2332f7c33510774
Armando Herrera <armandoher01@gmail.com>
2022-09-27 13:48:20 -0300

    fix: fix props on transaction detail

--- src/lib/pages/transaction/index.tsx
+++ src/lib/pages/transaction/index.tsx
@@ -1,7 +1,7 @@
 import TransactionDetail from "lib/components/transactiondetail/TransactionDetail";
 
 const Transaction = () => {
-  return <TransactionDetail />;
+  return <TransactionDetail transaction_hash={[]} />;
 };
 
 export default Transaction;

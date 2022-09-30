412550acdbb2c438b9400bde47f591ed9aa32049
Armando Herrera <armandoher01@gmail.com>
2022-09-30 15:12:01 -0300

    feat: create token page component

---
+++ src/lib/pages/token/index.tsx
@@ -0,0 +1,15 @@
+import TokenDetail from "lib/components/tokendetail/TokenDetail";
+import TokenInputData from "lib/components/tokeninputdata/TokenInputData";
+import TokenList from "lib/components/tokenlist/TokenList";
+
+const Token = () => {
+  return (
+    <>
+      <TokenDetail />
+      <TokenList />
+      <TokenInputData />
+    </>
+  );
+};
+
+export default Token;

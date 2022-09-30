2cf2e8ea93edc9e5368c00fb9e82a8a724000c86
Armando Herrera <armandoher01@gmail.com>
2022-09-30 15:09:57 -0300

    feat: create transfer data

---
+++ src/demodata/TransferData.tsx
@@ -0,0 +1,143 @@
+// Example list of blocks, to replace it with many.js block list get method
+const TransferData = () => {
+  return [
+    {
+      id: 1,
+      block_hash: {
+        value: "#457676765",
+        link: "/block/457676765",
+      },
+      time: "2hs 36min ago",
+      to: {
+        value: "0x457676765",
+        link: "/address/5435364646667",
+      },
+      from: {
+        value: "0x457676765",
+        link: "/address/5435364646667",
+      },
+      type: "Buy",
+    },
+    {
+      id: 2,
+      block_hash: {
+        value: "#457676766",
+        link: "/block/457676766",
+      },
+      time: "4hs 15mins ago",
+      from: {
+        value: "0x457676766",
+        link: "/address/5435364646668",
+      },
+      to: {
+        value: "0x457676766",
+        link: "/address/5435364646668",
+      },
+      type: "Sell",
+    },
+    {
+      id: 3,
+      block_hash: {
+        value: "#457676767",
+        link: "/block/457676767",
+      },
+      time: "4hs 54mins ago",
+      to: {
+        value: "0x457676767",
+        link: "/address/5435364646669",
+      },
+      from: {
+        value: "0x457676767",
+        link: "/address/5435364646669",
+      },
+      type: "Sign",
+    },
+    {
+      id: 4,
+      block_hash: {
+        value: "#457676768",
+        link: "/block/457676768",
+      },
+      time: "6hs 46mins ago",
+      to: {
+        value: "0x457676768",
+        link: "/address/5435364646670",
+      },
+      from: {
+        value: "0x457676768",
+        link: "/address/5435364646670",
+      },
+      type: "Execute",
+    },
+    {
+      id: 5,
+      block_hash: {
+        value: "#457676769",
+        link: "/block/457676769",
+      },
+      time: "4hs 16mins ago",
+      from: {
+        value: "0x457676769",
+        link: "/address/5435364646671",
+      },
+      to: {
+        value: "0x457676769",
+        link: "/address/5435364646671",
+      },
+      type: "Buy",
+    },
+    {
+      id: 6,
+      block_hash: {
+        value: "#457676770",
+        link: "/block/457676770",
+      },
+      time: "4hs 54mins ago",
+      to: {
+        value: "0x457676770",
+        link: "/address/5435364646672",
+      },
+      from: {
+        value: "0x457676770",
+        link: "/address/5435364646672",
+      },
+      type: "Sign",
+    },
+    {
+      id: 7,
+      block_hash: {
+        value: "#457676771",
+        link: "/block/457676771",
+      },
+      time: "6hs 46mins ago",
+      to: {
+        value: "0x457676771",
+        link: "/address/5435364646673",
+      },
+      from: {
+        value: "0x457676771",
+        link: "/address/5435364646673",
+      },
+      type: "Execute",
+    },
+    {
+      id: 8,
+      block_hash: {
+        value: "#457676772",
+        link: "/block/457676772",
+      },
+      time: "4hs 16mins ago",
+      from: {
+        value: "0x457676772",
+        link: "/address/5435364646674",
+      },
+      to: {
+        value: "0x457676772",
+        link: "/address/5435364646674",
+      },
+      type: "Buy",
+    },
+  ];
+};
+
+export default TransferData();

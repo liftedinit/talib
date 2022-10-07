a306f18dce6ccbd02745ea4027ed7a46dfe7e87a
Maximiliano José Sorribas <mjsorribas@gmail.com>
2022-10-07 19:35:39 -0300

    feat: add home tests and snapshot

---
+++ src/lib/test/pages/home/home.test.tsx
@@ -0,0 +1,16 @@
+import renderer from "react-test-renderer";
+import { expect, test } from "vitest";
+
+import Home from "lib/pages/home/index";
+
+const toJson = (component: renderer.ReactTestRenderer) => {
+  const result = component.toJSON();
+  expect(result).toBeDefined();
+  return result as renderer.ReactTestRendererJSON;
+};
+
+test("Home", () => {
+  const component = renderer.create(<Home />);
+  const tree = toJson(component);
+  expect(tree).toMatchSnapshot();
+});

e481ba653de9fa0ac57e1562b3b40a6baa0a901e
Armando Herrera <armandoher01@gmail.com>
2022-10-14 11:04:24 -0300

    feat: fix ajna

--- src/lib/components/blocklist/blocklistmobile/BlockListMobile.tsx
+++ src/lib/components/blocklist/blocklistmobile/BlockListMobile.tsx
@@ -6,16 +6,10 @@ import {
   PaginationPrevious,
   PaginationContainer,
   PaginationPageGroup,
+  PaginationPage,
 } from "@ajna/pagination";
-import {
-  Box,
-  Stack,
-  Text,
-  Table,
-  Tr,
-  Td,
-  ChakraProvider,
-} from "@chakra-ui/react";
+import { Box, Text, ChakraProvider } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 import { NoContent } from "../../customtable/components/NoContent";
 import type Block from "lib/types/block";
@@ -25,11 +19,31 @@ interface Props {
 }
 
 const BlockListMobile = ({ blocks }: Props) => {
+  // constants
+
+  const [totalItems, setTotalItems] = useState<number | undefined>(undefined);
+  const [listItems, setListItems] = useState<any[]>([]);
+
   // Control current Page
-  const { currentPage, setCurrentPage, pagesCount } = usePagination({
-    pagesCount: 1,
-    initialState: { currentPage: 1 },
-  });
+  const { pages, pagesCount, offset, currentPage, setCurrentPage, pageSize } =
+    usePagination({
+      total: totalItems,
+      initialState: {
+        currentPage: 1,
+        pageSize: 4,
+      },
+    });
+
+  // effects
+  useEffect(() => {
+    setTotalItems(blocks.length);
+    setListItems(blocks);
+  }, [currentPage, pageSize, offset, blocks]);
+
+  const handlePageChange = (nextPage: number): void => {
+    // -> request new data using the page number
+    setCurrentPage(nextPage);
+  };
 
   if (blocks.length === 0) {
     return <NoContent text="No information to show." />;
@@ -37,61 +51,30 @@ const BlockListMobile = ({ blocks }: Props) => {
 
   return (
     <Box>
-      {blocks.map((block) => (
-        <Stack key={block.id}>
-          <Table
-            variant="unstyled"
-            borderBottom="1px"
-            borderBottomColor="gray.200"
-          >
-            <thead />
-            <tbody>
-              <Tr p="1rem">
-                <Td>
-                  <Text color="#654D43" fontSize="12px" fontWeight={400}>
-                    BLOCK
-                  </Text>
-                </Td>
-                <Td isNumeric>{block.block_hash.value}</Td>
-              </Tr>
-              <Tr p="1rem">
-                <Td>
-                  <Text color="#654D43" fontSize="12px" fontWeight={400}>
-                    TIME
-                  </Text>
-                </Td>
-                <Td>{block.time}</Td>
-              </Tr>
-              <Tr p="1rem">
-                <Td>
-                  <Text color="#654D43" fontSize="12px" fontWeight={400}>
-                    NUMBER OF TXS
-                  </Text>
-                </Td>
-                <Td isNumeric>{block.num_of_txs}</Td>
-              </Tr>
-            </tbody>
-            <tfoot />
-          </Table>
-        </Stack>
-      ))}
-
       <ChakraProvider>
+        {listItems?.map((block: Block) => (
+          <Box key={block.id}>{block.id}</Box>
+        ))}
+
         <Pagination
           pagesCount={pagesCount}
           currentPage={currentPage}
-          onPageChange={setCurrentPage}
+          onPageChange={handlePageChange}
         >
           <PaginationContainer>
-            <PaginationPrevious>Previous</PaginationPrevious>
-            <PaginationPageGroup />
+            <PaginationPrevious>{"<"}</PaginationPrevious>
+            <PaginationPageGroup>
+              {pages.map((page: number) => (
+                <PaginationPage key={`pagination_page_${page}`} page={page} />
+              ))}
+            </PaginationPageGroup>
             <PaginationNext
               _hover={{
                 bg: "yellow.400",
               }}
               bg="yellow.300"
             >
-              <Text>Next</Text>
+              <Text>{">"}</Text>
             </PaginationNext>
           </PaginationContainer>
         </Pagination>

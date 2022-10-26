e20d33ed7024f396aa816edb75aab4106bf7f26e
Maximiliano José Sorribas <mjsorribas@gmail.com>
2022-10-26 13:06:18 -0300

    feat: update api config file, add endpoint, add default configurations

--- src/lib/config/api.ts
+++ src/lib/config/api.ts
@@ -1,3 +1,16 @@
-const Api = { blockinformation: "http://localhost:3000/blockinformation" };
+const Api = {
+  endpoint: {
+    blockinformation: "http://localhost:3000/blockinformation",
+    blocklist: "http://localhost:3000/blocklist",
+  },
+  options: {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "X-API-KEY": "1ab2c3d4e5f61ab2c3d4e5f6",
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+};
 
 export default Api;

f742c1b7ef2db2f84b5496c1de2e886b24d5f27b
Maximiliano José Sorribas <mjsorribas@gmail.com>
2022-11-17 15:23:21 -0300

    feat: update BlockNetworkInformation component, clean ui, add api calls

--- src/lib/components/blocknetwork/BlockNetworkInformation.tsx
+++ src/lib/components/blocknetwork/BlockNetworkInformation.tsx
@@ -1,265 +1,105 @@
-import {
-  Box,
-  Center,
-  Grid,
-  GridItem,
-  Image,
-  SimpleGrid,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
+import { HStack, VStack, Box, Heading, Icon, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 // import type BlockInformation from "lib/types/blockInformation";
+import { FaTrophy, FaCube, FaCode } from "react-icons/all";
+
+import Hcard from "../hcard/Hcard";
 import Api from "lib/config/api";
 import { theme } from "lib/styles/customTheme";
 
+const CircleIcon = (props) => (
+  <Icon viewBox="0 0 200 200" {...props}>
+    <path
+      fill="currentColor"
+      d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
+    />
+  </Icon>
+);
+
 const BlockNetworkInformation = () => {
   // Defined data states
-  const [TsxPerSec, setTsxPerSec] = useState(0);
-  const [TsxTps, setTsxTps] = useState(0);
-  const [BlockHeight, setBlockHeight] = useState(0);
-  const [HashRate, setHashRate] = useState(0);
-  const [ComputerPowerUsed, setComputerPowerUsed] = useState(0);
-  const [ComputerPowerAvailable, setComputerPowerAvailable] = useState(0);
-  const [ActiveNodes, setActiveNodes] = useState(0);
-  const [TotalNodes, setTotalNodes] = useState(0);
-  // GET Blockchain Networkinformation from API
-  const getBlockchainInfo = async () => {
+  const [Id, setId] = useState(0);
+  const [Neighborhood, setNeighborhood] = useState(0);
+  const [ServerName, setServerName] = useState(0);
+  const [Address, setAddress] = useState(0);
+  const [Attributes, setAttributes] = useState(0);
+  const [ServerVersion, setServerVersion] = useState(0);
+  const [ProtocolVersion, setProtocolVersion] = useState(0);
+  const [TimeDeltaInSecs, setTimeDeltaInSecs] = useState(0);
+  const [PublicKey, setPublicKey] = useState(0);
+  const [NetworkData, setNetworkData] = useState(0);
+  const [Created_at, setCreated_at] = useState(0);
+  const [Updated_at, setUpdated_at] = useState(0);
+
+  // GET Blockchain Network Information from API
+  const getNetworkInformation = async () => {
     try {
-      const response = await fetch(Api.endpoint.blockinformation, Api.options);
+      const response = await fetch(
+        Api.endpoint.networkinformation,
+        Api.options
+      );
       const data = await response.json();
-      setTsxPerSec(data.tsx_per_sec);
-      setTsxTps(data.tsx_tps);
-      setBlockHeight(data.block_height);
-      setHashRate(data.hash_rate);
-      setComputerPowerUsed(data.computer_power_used);
-      setComputerPowerAvailable(data.computer_power_available);
-      setActiveNodes(data.active_nodes);
-      setTotalNodes(data.total_nodes);
+      setId(data[0].id);
+      setNeighborhood(data[0].neighborhood);
+      setServerName(data[0].serverName);
+      setAddress(data[0].address);
+      setAttributes(data[0].serverVersion);
+      setServerVersion(data[0].serverVersion);
+      setProtocolVersion(data[0].protocolVersion);
+      setTimeDeltaInSecs(data[0].timeDeltaInSecs);
+      setPublicKey(data[0].publicKey);
+      setNetworkData(data[0].networkData);
+      setCreated_at(data[0].created_at);
+      setUpdated_at(data[0].updated_at);
     } catch (e) {
       // eslint-disable-next-line no-console
       console.log(e);
     }
   };
   useEffect(() => {
-    getBlockchainInfo();
+    getNetworkInformation();
   });
 
   return (
-    <Center h={{ sm: "681px", md: "200px" }}>
-      <Grid
-        pos="absolute"
-        top="300px"
-        h={{ sm: "650px", md: "177px" }}
-        w={{ sm: "23em", md: "48em", lg: "72em" }}
-        maxW="full"
-        templateColumns={{ sm: "repeat(1, 1fr)", md: "repeat(3, 1fr)" }}
-        gap={2}
-        bg={theme.colors.white}
-        borderRadius="md"
-        alignItems={{ sm: "start", md: "center" }}
-        paddingTop={{ sm: "30px", md: "0px" }}
-      >
-        <GridItem
-          colSpan={2}
-          h={{ sm: "325px", md: "177px" }}
-          alignItems="center"
-        >
-          <SimpleGrid
-            columns={{ sm: 1, md: 2 }}
-            borderBottom={{ sm: "none", md: "1px" }}
-            borderColor={{ md: "gray.300" }}
-            spacing={0}
-            height={{ sm: "175", md: "88px" }}
-            paddingLeft={{ sm: "0px", md: "25px" }}
+    <VStack maxW="full">
+      <VStack maxW="full" alignItems="left">
+        <Box>
+          <Heading
+            fontSize="2.25rem"
+            fontWeight={600}
+            as="h1"
+            color={theme.colors.cream}
           >
-            <Box
-              height="88px"
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
-              paddingLeft={{ sm: "25px", md: "0px" }}
-              alignItems={{ sm: "start", md: "start" }}
-            >
-              <Stack display="flex" flexDirection="row" alignItems="center">
-                <Text fontSize="24px" fontWeight="400" lineHeight="28px">
-                  {TsxPerSec}M
-                </Text>
-                <Text
-                  color={theme.colors.green}
-                  fontWeight="400"
-                  lineHeight="20px"
-                  fontSize="14px"
-                >
-                  ({TsxTps} tps)
-                </Text>
-              </Stack>
-              <Text
-                color={theme.colors.brown}
-                fontWeight="400"
-                lineHeight="20px"
-                fontSize="14px"
-              >
-                TRANSACTIONS PER SECOND
-              </Text>
-            </Box>
-            <Box
-              height="88px"
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
-              paddingLeft={{ sm: "25px", md: "0px" }}
-              alignItems={{ sm: "start", md: "start" }}
-            >
-              <Stack display="flex" flexDirection="row" alignItems="center">
-                <Text fontSize="24px" fontWeight="400" lineHeight="28px">
-                  {BlockHeight}
-                </Text>
-                <Text
-                  color={theme.colors.green}
-                  fontWeight="400"
-                  lineHeight="20px"
-                  fontSize="14px"
-                />
-              </Stack>
-              <Text
-                color={theme.colors.brown}
-                fontWeight="400"
-                lineHeight="20px"
-                fontSize="14px"
-              >
-                BLOCK HEIGHT
-              </Text>
-            </Box>
-          </SimpleGrid>
-          <SimpleGrid
-            spacing={0}
-            height={{ sm: "175", md: "88px" }}
-            columns={{ sm: 1, md: 2 }}
-            paddingLeft={{ sm: "0px", md: "25px" }}
-          >
-            <Box
-              height="88px"
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
-              paddingLeft={{ sm: "25px", md: "0px" }}
-              alignItems={{ sm: "start", md: "start" }}
-            >
-              <Stack display="flex" flexDirection="row" alignItems="center">
-                <Text fontSize="24px" fontWeight="400" lineHeight="28px">
-                  {HashRate} GH/s
-                </Text>
-                <Text
-                  color={theme.colors.green}
-                  fontWeight="400"
-                  lineHeight="20px"
-                  fontSize="14px"
-                />
-              </Stack>
-              <Text
-                color={theme.colors.brown}
-                fontWeight="400"
-                lineHeight="20px"
-                fontSize="14px"
-              >
-                HASH RATE
-              </Text>
-            </Box>
-            <Box
-              height="88px"
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
-              paddingLeft={{ sm: "25px", md: "0px" }}
-              alignItems={{ sm: "start", md: "start" }}
-            >
-              <Stack display="flex" flexDirection="row" alignItems="center">
-                <Text fontSize="24px" fontWeight="400" lineHeight="28px">
-                  {ComputerPowerUsed}
-                </Text>
-                <Text
-                  color={theme.colors.green}
-                  fontWeight="400"
-                  lineHeight="20px"
-                  fontSize="14px"
-                >
-                  {ComputerPowerAvailable}
-                </Text>
-              </Stack>
-              <Text
-                color={theme.colors.brown}
-                fontWeight="400"
-                lineHeight="20px"
-                fontSize="14px"
-              >
-                COMPUTER POWER USED / AVAILABLE
-              </Text>
-            </Box>
-          </SimpleGrid>
-        </GridItem>
-        <GridItem
-          colSpan={1}
-          rowSpan={2}
-          borderLeft={{ sm: "none", md: "1px" }}
-          borderColor={{ md: "gray.300" }}
-          h={{ sm: "325px", md: "177px" }}
-        >
-          <SimpleGrid
-            columns={{ sm: 1, md: 1 }}
-            spacing={0}
-            height={{ sm: "88px", md: "80px" }}
-            paddingLeft={{ sm: "0px", md: "25px" }}
-          >
-            <Box
-              height="88px"
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
-              paddingLeft={{ sm: "25px", md: "0px" }}
-              alignItems={{ sm: "start", md: "start" }}
-            >
-              <Stack display="flex" flexDirection="row" alignItems="center">
-                <Text fontSize="24px" fontWeight="400" lineHeight="28px">
-                  {ActiveNodes}/{TotalNodes}
-                </Text>
-                <Text
-                  color={theme.colors.green}
-                  fontWeight="400"
-                  lineHeight="20px"
-                  fontSize="14px"
-                />
-              </Stack>
-              <Text
-                color={theme.colors.brown}
-                fontWeight="400"
-                lineHeight="20px"
-                fontSize="14px"
-              >
-                ACTIVE NODES
-              </Text>
-            </Box>
-          </SimpleGrid>
-          <SimpleGrid
-            spacing={0}
-            height={{ sm: "175", md: "88px" }}
-            columns={{ sm: 1, md: 1 }}
-            paddingLeft="25px"
-          >
-            <Box
-              height={{ sm: "175", md: "88px" }}
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
-              alignItems={{ sm: "center", md: "start" }}
-            >
-              <Image objectFit="cover" src="/assets/map.png" alt="map" />
-            </Box>
-          </SimpleGrid>
-        </GridItem>
-      </Grid>
-    </Center>
+            {String(Neighborhood)}
+          </Heading>
+        </Box>
+        <Box as="span" display="flex">
+          <CircleIcon color={theme.colors.green} boxSize={6} />
+          <Text color={theme.colors.cream} fontWeight={900} fontSize="0.75rem">
+            currently available
+          </Text>
+        </Box>
+      </VStack>
+      <HStack mt="70px" maxW="full">
+        <Hcard
+          title="Block Height "
+          icon={FaCube}
+          content={String(ProtocolVersion)}
+        />
+
+        <Hcard
+          title="Protocol Version"
+          icon={FaTrophy}
+          content={String(ProtocolVersion)}
+        />
+        <Hcard
+          title="Software Version"
+          icon={FaCode}
+          content={String(ServerVersion)}
+        />
+      </HStack>
+    </VStack>
   );
 };
 

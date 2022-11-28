d9d22bb9907340e3a634d7220fd2e415b27c002f
Maximiliano José Sorribas <mjsorribas@gmail.com>
2022-11-28 16:14:57 -0300

    feat: update blockinformationmock, update data of blockchain.info

--- src/mocks/request/blockinformationmock.tsx
+++ src/mocks/request/blockinformationmock.tsx
@@ -1,14 +1,26 @@
 // Mock Data
 const BlockinformationMock = [
   {
-    status: {
-      protocolVersion: 1,
-      serverName: "AbciModule(many-ledger)",
-      publicKey: {},
-      address: "mahpfjaf27syb5ffukce3m3p4mcuwpdjpijf2bbxvizy4trijw",
-      attributes: [0, 1, 2, 4, 5, 6, 8, [9, 0, 1], 1002],
-      serverVersion: "0.1.0",
-      timeDeltaInSecs: 300,
+    info: {
+      latestBlock: {
+        hash: {
+          type: "Buffer",
+          data: [
+            146, 192, 224, 66, 155, 82, 69, 114, 94, 125, 110, 185, 93, 24, 68,
+            40, 120, 176, 187, 42, 57, 172, 212, 90, 64, 65, 46, 139, 86, 60,
+            45, 49,
+          ],
+        },
+        height: 1162,
+      },
+      appHash: {
+        type: "Buffer",
+        data: [
+          31, 192, 49, 182, 151, 139, 222, 146, 215, 196, 55, 243, 250, 28, 116,
+          180, 11, 175, 52, 53, 191, 113, 185, 46, 115, 73, 155, 11, 234, 74,
+          150, 2,
+        ],
+      },
     },
   },
 ];

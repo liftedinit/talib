0ed8808a424f9975f2442dab4c2ab4c99257016e
Maximiliano José Sorribas <mjsorribas@gmail.com>
2022-11-28 16:11:25 -0300

    feat: update networkstatus, update data of  network status

---
+++ src/mocks/request/networkstatus.tsx
@@ -0,0 +1,15 @@
+// Mock Data
+const NetworkStatusMock = [
+  {
+    status: {
+      protocolVersion: 1,
+      serverName: "AbciModule(many-ledger)",
+      publicKey: {},
+      address: "mahpfjaf27syb5ffukce3m3p4mcuwpdjpijf2bbxvizy4trijw",
+      attributes: [0, 1, 2, 4, 5, 6, 8, [9, 0, 1], 1002],
+      serverVersion: "0.1.0",
+      timeDeltaInSecs: 300,
+    },
+  },
+];
+export default NetworkStatusMock;

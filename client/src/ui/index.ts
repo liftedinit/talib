aea728fdcb8416977158c045f6145cf763681a18
Stanley Jones <stanley@sunshocked.com>
2023-03-24 11:02:45 -0700

    feat: Search bar on homepage (#208)

--- client/src/ui/index.ts
+++ client/src/ui/index.ts
@@ -1,5 +1,6 @@
 export * from "./block-list";
 export * from "./error-alert";
 export * from "./pager";
+export * from "./search";
 export * from "./stat";
 export * from "./txn-list";
---
+++ client/src/ui/search.tsx
@@ -0,0 +1,53 @@
+import {
+  CheckIcon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  SearchIcon,
+} from "@liftedinit/ui";
+import { FormEvent, useState } from "react";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+
+const validTerms = [
+  { path: "blocks", regex: /^\d+$/ },
+  { path: "transactions", regex: /^[0-9a-f]{64}$/ },
+  { path: "address", regex: /^m[0-9a-zA-Z]{49,54}$/ },
+];
+
+function isValid(term: string) {
+  term = term.replace(/[^0-9a-z]/gi, "");
+  return validTerms.some((t) => t.regex.test(term));
+}
+
+function maybeNavigate(term: string, navigate: NavigateFunction) {
+  term = term.replace(/[^0-9a-z]/gi, "");
+  return function (e: FormEvent) {
+    e.preventDefault();
+    for (let t of validTerms) {
+      if (t.regex.test(term)) {
+        navigate(`/${t.path}/${term}`);
+      }
+    }
+  };
+}
+
+export function Search() {
+  const [term, setTerm] = useState("");
+  const navigate = useNavigate();
+
+  return (
+    <form onSubmit={maybeNavigate(term, navigate)}>
+      <InputGroup bg="white">
+        <InputLeftElement children={<SearchIcon />} />
+        <Input
+          placeholder="Search by block height, transaction hash, or address"
+          onChange={(e) => setTerm(e.target.value)}
+        />
+        {isValid(term) && (
+          <InputRightElement children={<CheckIcon color="brand.teal.500" />} />
+        )}
+      </InputGroup>
+    </form>
+  );
+}

6dff0730ec6bd43dde8bc11613321b9b19ea69b6
Justin <justin@justingriffin.com>
2023-05-20 15:09:09 -0500

    Adding free disk space Fixing spinner to check for isLoading on all metrics before displaying stack

--- client/src/pages/home/metrics.tsx
+++ client/src/pages/home/metrics.tsx
@@ -7,43 +7,61 @@ import { Stat } from "ui";
 export function NetworkMetrics() {
 
   // Get a list of all stats
-  const { data } = useQuery(["stats"], getManifestMetrics());
+  const getNodeCountQuery = useQuery(["nodecount"], getManifestMetricCurrent("nodecount"));
+  const getCpuCoresQuery = useQuery(["cpucores"], getManifestMetricCurrent("cpucores"));
+  const getTotalBlocksQuery = useQuery(["totalblocks"], getManifestMetricCurrent("totalblocks"));
+  const getFreeMemoryQuery = useQuery(["freememory"], getManifestMetricCurrent("freememory"));
+  const getUsedMemoryQuery = useQuery(["usedmemory"], getManifestMetricCurrent("usedmemory"));
+  const getFreeDiskSpaceQuery = useQuery(["freediskspace"], getManifestMetricCurrent("freediskspace"));
+  const getUsedDiskSpaceQuery = useQuery(["useddiskspace"], getManifestMetricCurrent("useddiskspace"));
+  const getTotalDiskSpaceQuery = useQuery(["totaldiskspace"], getManifestMetricCurrent("totaldiskspace"));
 
-  const getNodeCount = useQuery(["nodecount"], getManifestMetricCurrent("nodecount")).data;
-  const getCpuCores = useQuery(["cpucores"], getManifestMetricCurrent("cpucores")).data;
-  const getTotalBlocks = useQuery(["totalblocks"], getManifestMetricCurrent("totalblocks")).data;
-  const getFreeMemory = useQuery(["freememory"], getManifestMetricCurrent("freememory")).data;
-  const getUsedMemory = useQuery(["usedmemory"], getManifestMetricCurrent("usedmemory")).data;
+  const getNodeCount = getNodeCountQuery.data;
+  const getCpuCores = getCpuCoresQuery.data;
+  const getTotalBlocks = getTotalBlocksQuery.data;
+  const getFreeMemory = getFreeMemoryQuery.data;
+  const getUsedMemory = getUsedMemoryQuery.data;
+  const getFreeDiskSpace = getFreeDiskSpaceQuery.data;
+  const getUsedDiskSpace = getUsedDiskSpaceQuery.data; 
+  const getTotalDiskSpace = getTotalDiskSpaceQuery.data; 
 
   let convertToGb = (b: number): number => {
     let gbValue = (b / (1000));
     return gbValue
   }
 
-  let convertToPb = (b: number): number => {
+  let convertToTb = (b: number): number => {
     let gbValue = (b / (1000 * 1000));
     return gbValue
   }
 
-  if (!data) {
+  if ( getNodeCountQuery.isLoading || 
+    getCpuCoresQuery.isLoading || 
+    getTotalBlocksQuery.isLoading || 
+    getFreeMemoryQuery.isLoading || 
+    getUsedMemoryQuery.isLoading ||
+    getFreeDiskSpaceQuery.isLoading ||
+    getUsedDiskSpaceQuery.isLoading )  {
     return <Spinner />;
   }
 
   return (   
     <>
-      {data.isLoading ? (
-        <Spinner />
-      ) : (     
     <><Stack direction="row" mt={6}>
         <Stat label="Total Nodes" value={getNodeCount} />
         <Stat label="Total CPU Cores" value={getCpuCores} />
         <Stat label="Total Blocks Produced" value={parseInt(getTotalBlocks)} />
       </Stack>
       <Stack direction="row" mt={6}>
-        <Stat label="Free Memory" value={Math.floor(convertToPb(getFreeMemory)) + " TB"} />
+        <Stat label="Free Memory" value={Math.floor(convertToTb(getFreeMemory)) + " TB"} />
         <Stat label="Used Memory" value={Math.floor(convertToGb(getUsedMemory)) + " GB"} />
-      </Stack></>
-    )}
+      </Stack>
+      <Stack direction="row" mt={6}>
+        <Stat label="Total Disk Space" value={Number(getTotalDiskSpace / 1000 / 1000).toFixed(5) + " PB"} />
+        <Stat label="Free Disk Space" value={Number(getFreeDiskSpace / 1000).toFixed(2) + " TB"} />
+        <Stat label="Used Disk Space" value={Number(getUsedDiskSpace / 1000).toFixed(2) + " TB"} />
+      </Stack>
+      </>
   </>
   );
 }

ec76d8da1653bd9c1acca84e83af66ca9a7a6af4
Justin <justin@justingriffin.com>
2023-08-22 10:16:48 -0500

    Removed series endpoint for now

--- server/src/metrics/systemwide/systemwide.controller.ts
+++ server/src/metrics/systemwide/systemwide.controller.ts
@@ -27,40 +27,6 @@ export class SystemWideController {
     return this.systemwide.getTotalBlocks();
   }
 
-  @Get("totalblocks/series")
-  @ApiQuery({ name: "from", required: false })
-  @ApiQuery({ name: "to", required: false })
-  @ApiQuery({ name: "hours", required: false })
-  getBlocksTotalSeries(
-    @Query("from") from?: string,
-    @Query("to") to?: string,
-    @Query("hours") hours?: number,
-  ): Promise<SeriesEntity[]> {
-    if (!hours) {
-      hours = 24;
-    }
-    let currentDate: Date;
-    let previousDate: Date;
-
-    if (!from) {
-      currentDate = new Date();
-    } else {
-      currentDate = new Date();
-    }
-    if (!to) {
-      previousDate = new Date();
-      previousDate.setHours(previousDate.getHours() - hours);
-    } else {
-      const fromParts = from.split("-");
-      const fromDays = Number(fromParts[1].replace("d", ""));
-      const fromHours = fromDays * 24;
-      previousDate = new Date();
-      previousDate.setHours(previousDate.getHours() - fromHours);
-    }
-
-    return this.systemwide.getTotalBlocksSeries(currentDate, previousDate);
-  }
-
   @Get("totaltransactions/current")
   getTransactionsTotalCurrent() {
     return this.systemwide.getTotalTransactions();

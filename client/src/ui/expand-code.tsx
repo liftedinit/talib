bd0a9f08c69119ff22bbfd2e81650b6cb8f14400
Justin <justin@justingriffin.com>
2023-09-12 16:47:18 -0500

    Adding expand-code component to client

---
+++ client/src/ui/expand-code.tsx
@@ -0,0 +1,40 @@
+import { Code, Button, Box  } from "@liftedinit/ui";
+import { useState } from "react";
+
+export function ExpandCode({ content }: any) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
+  }
+
+  const ExpandButton = () => (
+    <Button as="a" variant="link" size="sm" ml={1} onClick={toggleExpanded} color="brand.teal.500">
+      {isExpanded ? "(Collapse)" : "(...)"}
+    </Button>
+  );
+
+  return (
+    <Box bg="white" my={6}>
+      {
+        isExpanded ? (
+          <Code maxW="50em">
+            {content}
+            <ExpandButton />
+          </Code>
+        ) : content.length > 5000 ? (
+          <>
+            <Code maxW="50em">
+              {content.substring(0, 5000)}
+              <ExpandButton />
+            </Code>
+          </>
+        ) : (
+          <Code maxW="50em">
+            {content}
+          </Code>
+        )
+      }
+    </Box>
+  );
+}

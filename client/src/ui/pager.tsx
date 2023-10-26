f3f3dc97b1ff6ad048a6be59d0e7cafc06bac18d
Justin Griffin <jgriffin555@comcast.net>
2023-10-26 17:11:49 -0500

    feat: Update pager to use button color mode bg

--- client/src/ui/pager.tsx
+++ client/src/ui/pager.tsx
@@ -1,4 +1,5 @@
 import { Button, ButtonGroup, Center } from "@liftedinit/ui";
+import { useButtonBg } from "utils";
 
 interface PagerProps {
   page: number;
@@ -7,15 +8,18 @@ interface PagerProps {
 }
 
 export function Pager({ page, setPage, totalPages }: PagerProps) {
+  const buttonBg = useButtonBg();
+
   return (
     <Center my={6}>
       <ButtonGroup isAttached>
-        <Button onClick={() => setPage(page - 1)} isDisabled={page === 1}>
+        <Button onClick={() => setPage(page - 1)} isDisabled={page === 1} bg={buttonBg}>
           Previous
         </Button>
         <Button
           onClick={() => setPage(page + 1)}
           isDisabled={page >= totalPages}
+          bg={buttonBg}
         >
           Next
         </Button>

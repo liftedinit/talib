7b3edad41d149c1d4e4f56a9cc452495ae97771d
Justin Griffin <jgriffin555@comcast.net>
2024-01-12 12:31:30 -0600

    feat: Adding navbar to metrics page
    Moved map component to the top of the page
    Simplified MetricNav Box container, removed absolute positioning
    Added NavItems array for sidebar menu
    Removed heading for Metrics section, redundant contextually

--- client/src/pages/metrics/metrics.tsx
+++ client/src/pages/metrics/metrics.tsx
@@ -1,24 +1,48 @@
-import { Heading, SimpleGrid, Center} from "@liftedinit/ui";
-import { MapChart } from "ui";
+import { Box, Heading, SimpleGrid, Center, Grid, GridItem } from "@liftedinit/ui";
+import { MapChart, MetricNav } from "ui";
 import { NetworkMetrics } from "./networkMetrics";
+import { MdMemory, MdStorage, MdDns} from "react-icons/md";
+import { FaHive, FaNetworkWired, FaFile, FaServer, FaGhost } from "react-icons/fa6"
+
+const navItems = [
+  { label: "Neighborhoods", section: "blockchain", icon: FaHive },
+  { label: "Nodes", section: "nodes", icon: FaServer },
+  { label: "Web", section: "web", icon: MdDns },
+  { label: "Hosting", section: "hosting", icon: FaGhost },
+  { label: "Storage", section: "storage", icon: MdStorage },
+  { label: "Memory", section: "memory", icon: MdMemory },
+  { label: "Network", section: "network", icon: FaNetworkWired },
+  { label: "Object Storage", section: "object-storage", icon: FaFile },
+  // { label: "AI", section: "ai", icon: MdRobot },
+];
 
 export function Metrics() {
   return (
     <>
-      <Center my={12}>
-      <Heading fontWeight="light">
-        Metrics for the <b>Manifest</b> network
-      </Heading>
-    </Center>
-    <NetworkMetrics />
-    <Center mt={12}>
-      <Heading fontWeight="light">
-        Nodes on the <b>Manifest</b> network
-      </Heading>
-    </Center>
-    <SimpleGrid gap="3" mt={2}>
-      <MapChart />
-    </SimpleGrid>
-    </>
+    <Grid gap={3}>
+        <Center mt={12}>
+          <Heading fontWeight="light">
+            Nodes on the <b>Manifest</b> network
+          </Heading>
+        </Center>
+      <GridItem>
+        <SimpleGrid gap="3" mt={2} id="map">
+          <MapChart />
+        </SimpleGrid>
+      </GridItem>
+    </Grid>
+    <Grid gap={3} templateColumns={{base: "1fr", md: "12rem 1fr"}} >
+      <GridItem>
+        <Box>
+          <MetricNav navItems={navItems} />
+        </Box>
+      </GridItem>
+      <GridItem>
+        <NetworkMetrics />
+      </GridItem>
+    </Grid>
+  </>
   );
 }
+
+

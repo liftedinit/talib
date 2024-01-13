3abd0e7bc542408465ae26a238af87aa752774de
Justin Griffin <jgriffin555@comcast.net>
2024-01-12 21:30:11 -0600

    feat: Include metricNav

--- client/src/ui/index.ts
+++ client/src/ui/index.ts
@@ -2,6 +2,7 @@ export * from "./block-list";
 export * from "./error-alert";
 export * from "./metricChart";
 export * from "./metricStat";
+export * from "./metricNav";
 export * from "./object-table";
 export * from "./pager";
 export * from "./query-box";

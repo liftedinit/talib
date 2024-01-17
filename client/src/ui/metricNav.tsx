2fb715394d73ced322b7105c7a4a0614f3baa5a2
Justin Griffin <jgriffin555@comcast.net>
2024-01-16 18:20:16 -0600

    feat: Add dynamic rendering of nav items based on the link or section prop to use either smooth scroll component or hyperlink.

--- client/src/ui/metricNav.tsx
+++ client/src/ui/metricNav.tsx
@@ -1,24 +1,40 @@
-import { Center, HStack, Icon, Text, Stack } from "@liftedinit/ui"
+import { Center, HStack, Icon, Link, Text, Stack } from "@liftedinit/ui"
 import { IconType } from "@react-icons/all-files"
-import { Link, animateScroll as scroll } from "react-scroll";
+import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 
 interface NavItemProps {
-  section: string
+  section?: string
   label: string
   icon: IconType
+  link?: string
 }
 
-export function NavItem({ section, label, icon }: NavItemProps) {
+export function NavItem({ section, link, label, icon }: NavItemProps) {
 
+if (section) {
+    return (
+      <ScrollLink 
+        to={section}
+        activeClass="active"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >
+        <HStack m={0}>
+          <Center p={3}>
+            <Icon
+              as={icon}
+              aria-label={label}
+            />
+          </Center>
+          <Text >{label}</Text>
+        </HStack>
+      </ScrollLink>
+    );
+} else if (link) {
   return (
-    <Link 
-      to={section}
-      activeClass="active"
-      spy={true}
-      smooth={true}
-      offset={-70}
-      duration={500}
-    >
+    <Link href={link}>
       <HStack m={0}>
         <Center p={3}>
           <Icon
@@ -29,7 +45,8 @@ export function NavItem({ section, label, icon }: NavItemProps) {
         <Text >{label}</Text>
       </HStack>
     </Link>
-  )
+  );
+}
 }
 
 interface NavProps {

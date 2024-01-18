37ffcc87e380862df5bfb08cade6108c0565a7b3
Justin Griffin <jgriffin555@comcast.net>
2024-01-18 08:51:52 -0600

    feat: Sticky nav
    Adding sticky nav to affix sidebar to top of the page when user scrolls.
    Add responsive query to remove nav when page width is small or below.

--- client/src/pages/metrics/metrics.tsx
+++ client/src/pages/metrics/metrics.tsx
@@ -1,10 +1,13 @@
-import { Box, Heading, SimpleGrid, Center, Grid, GridItem } from "@liftedinit/ui";
-import { MapChart, MetricNav } from "ui";
+import { useLayoutEffect, useState } from 'react';
+import { Box, Heading, Center, Grid, GridItem, useMediaQuery } from "@liftedinit/ui";
+import { MetricNav } from "ui";
 import { NetworkMetrics } from "./networkMetrics";
 import { MdMemory, MdStorage, MdDns} from "react-icons/md";
-import { FaHive, FaNetworkWired, FaFile, FaServer, FaGhost } from "react-icons/fa6"
+import { FaHive, FaNetworkWired, FaFile, FaServer, FaGhost, FaMap } from "react-icons/fa6"
+import { debounce } from 'lodash';
 
 const navItems = [
+  { label: "Map", link: "/#/map", icon: FaMap },
   { label: "Neighborhoods", section: "blockchain", icon: FaHive },
   { label: "Nodes", section: "nodes", icon: FaServer },
   { label: "Web", section: "web", icon: MdDns },
@@ -17,25 +20,50 @@ const navItems = [
 ];
 
 export function Metrics() {
+  const [isSticky, setSticky] = useState(false);
+
+  const [mobileWidth] = useMediaQuery('(max-width: 768px)');
+
+  const debouncedHandleScroll = debounce(() => {
+    const threshold = 100;
+    const offset = window.scrollY;
+    setSticky(offset > threshold);
+  }, 0);
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      setSticky(false);
+    };
+
+    window.addEventListener('scroll', debouncedHandleScroll);
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('scroll', debouncedHandleScroll);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [debouncedHandleScroll]);
+
   return (
     <>
     <Grid gap={3}>
         <Center mt={12}>
           <Heading fontWeight="light">
-            Nodes on the <b>Manifest</b> network
+            <b>Manifest</b> Network Metrics
           </Heading>
         </Center>
-      <GridItem>
-        <SimpleGrid gap="3" mt={2} id="map">
-          <MapChart />
-        </SimpleGrid>
-      </GridItem>
     </Grid>
     <Grid gap={3} templateColumns={{base: "1fr", md: "12rem 1fr"}} >
       <GridItem>
-        <Box>
-          <MetricNav navItems={navItems} />
+        {mobileWidth ? null : (
+        <Box 
+          position={isSticky ? 'fixed' : 'static'} 
+          top={10}  
+          className="metric-nav" 
+        >
+          <MetricNav  navItems={navItems} />
         </Box>
+        )}
       </GridItem>
       <GridItem>
         <NetworkMetrics />

d9df157687ad661ae49f610c8f6b11d613cf42e3
Justin Griffin <jgriffin555@comcast.net>
2024-01-18 08:52:36 -0600

    feat: Moving click handlers to MetricNav component to track index between all ScrollLink component items.

--- client/src/ui/metricNav.tsx
+++ client/src/ui/metricNav.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Center, HStack, Icon, Link, Text, Stack } from "@liftedinit/ui"
 import { IconType } from "@react-icons/all-files"
 import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
@@ -8,29 +8,46 @@ interface NavItemProps {
   label: string
   icon: IconType
   link?: string
+  onClick: () => void;
+  isActive: boolean;
 }
 
-export function NavItem({ section, link, label, icon }: NavItemProps) {
-  const [isActive, setIsActive ] = useState(false)
+export function NavItem({ section, link, label, icon, onClick, isActive }: NavItemProps) {
 
-  const handleClick = () => {
-    const active = document.getElementsByClassName("active")[0];
-    if (active) {
-      active.classList.remove("active");
-    }
-    setIsActive(true);
-  };
+  if (!section) {
+    return null; 
+  }
 
-if (section) {
+  if (section) {
+      return (
+        <ScrollLink 
+          to={section}
+          spy={false}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          onClick={() => {
+            onClick();
+          }}
+          className={isActive ? "active" : ""}
+        >
+          <HStack m={0} >
+            <Center p={3}>
+              <Icon
+                as={icon}
+                aria-label={label}
+              />
+            </Center>
+            <Text >{label}</Text>
+          </HStack>
+        </ScrollLink>
+      );
+  } else if (link) {
     return (
-      <ScrollLink 
-        to={section}
-        spy={false}
-        smooth={true}
-        offset={-70}
-        duration={500}
+      <Link 
+        href={link}
       >
-        <HStack m={0} onClick={handleClick} className={isActive ? "active" : ""}>
+        <HStack m={0}>
           <Center p={3}>
             <Icon
               as={icon}
@@ -39,25 +56,9 @@ if (section) {
           </Center>
           <Text >{label}</Text>
         </HStack>
-      </ScrollLink>
+      </Link>
     );
-} else if (link) {
-  return (
-    <Link 
-      href={link}
-    >
-      <HStack m={0}>
-        <Center p={3}>
-          <Icon
-            as={icon}
-            aria-label={label}
-          />
-        </Center>
-        <Text >{label}</Text>
-      </HStack>
-    </Link>
-  );
-}
+  }
 }
 
 interface NavProps {
@@ -65,9 +66,23 @@ interface NavProps {
 }
 
 export function MetricNav({ navItems }: NavProps) {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const handleItemClick = (index: number) => {
+    setActiveIndex(index);
+  };
+
   return (
-    <Stack mt={20}>
-      {navItems.map(NavItem)}
-    </Stack>
+  <Stack mt={20}>
+    {navItems.map((item, index) =>
+      NavItem({
+        section: item.section,
+        label: item.label,
+        icon: item.icon,
+        onClick: () => handleItemClick(index),
+        isActive: index === activeIndex,
+      })
+    )}
+  </Stack>
   )
 }

b9a0cb8cc6645f7af5b6b78565cb8b7470c996e1
Justin Griffin <jgriffin555@comcast.net>
2024-01-18 20:52:44 -0600

    feat: Replacing main menu link with TopNav component and links for metrics and map page. Removing metrics button and fixing alignment of network selector.

--- client/src/pages/layout/navbar.tsx
+++ client/src/pages/layout/navbar.tsx
@@ -1,26 +1,30 @@
 import {
   Box,
-  Button,
   Center,
   Grid,
   GridItem,
   Heading,
   Image,
   logoSvg,
-  Stack,
   HStack,
   DarkMode,
 } from "@liftedinit/ui";
-import { Link } from "react-router-dom";
-import { Search, DarkModeToggle } from "ui";
+import { Search, DarkModeToggle, TopNav } from "ui";
 import { useBgColor } from "utils";
 import { NeighborhoodSelector } from "./selector";
+
+const navItems = [
+  { label: "Talib", link: "/" },
+  { label: "Metrics", link: "/metrics"},
+  { label: "Map", link: "/map" },
+];
 
 export function Navbar() {
 
   const bg = useBgColor();
 
   return (
+    <>
     <Grid
     shadow="lg"
     bg={bg}
@@ -32,9 +36,7 @@ export function Navbar() {
     <GridItem>
       <HStack direction="row">
         <Image src={logoSvg} h="67px" mr={3} alt="Lifted Logo" />
-        <Heading lineHeight="67px" size="md" fontWeight="normal">
-          <Link to="/">Talib</Link>
-        </Heading>
+        <TopNav navItems={navItems} />
       </HStack>
     </GridItem>
     <GridItem>
@@ -45,18 +47,14 @@ export function Navbar() {
       </Center>
     </GridItem>
     <GridItem>
-      <HStack display="flex" justifyContent="space-between">
-        <Heading lineHeight="67px" size="md" fontWeight="normal" marginRight="20px" justify-self-end>
-          <Button colorScheme='brand.teal' size="md">
-          <Link to="/metrics">Metrics</Link>
-          </Button>
-        </Heading>
-        <NeighborhoodSelector justify-self-end />
-        <Heading size="md" justify-self-end>
+      <HStack justify="flex-end">
+        <NeighborhoodSelector />
+        <Heading size="md">
           <DarkModeToggle />
         </Heading>
       </HStack>
     </GridItem>
     </Grid>
+    </>
   );
 }

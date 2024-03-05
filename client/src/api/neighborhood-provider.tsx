d9b24cb03aef7cd1e6ecde18f52c97b4137bfc5f
Justin Griffin <jgriffin555@comcast.net>
2024-03-04 18:33:03 -0600

    feat: retrieve tokens info and store in neighborhood context

--- client/src/api/neighborhood-provider.tsx
+++ client/src/api/neighborhood-provider.tsx
@@ -1,17 +1,28 @@
 import { createContext, ReactNode, useState } from "react";
+import { useQuery  } from "@tanstack/react-query";
+import { getNeighborhoodTokens } from "./queries";
 
 const ID = import.meta.env.PROD ? 6 : 1; // Default to Manifest Ledger Alpha 2 (current) in production
 
 export const NeighborhoodContext = createContext({
   id: ID,
   setId: (_: number) => {},
+  tokens: [],
 });
 
 export function NeighborhoodProvider({ children }: { children: ReactNode }) {
   const [id, setId] = useState(ID);
 
+  const tokensQuery = useQuery(
+    ["neighborhoods", id, "tokens"],
+    getNeighborhoodTokens(id),
+  );
+
+  const { data, isError, isSuccess} = tokensQuery;
+  const tokens = isError || !data ? [] : data.items;
+
   return (
-    <NeighborhoodContext.Provider value={{ id, setId }}>
+    <NeighborhoodContext.Provider value={{ id, setId, tokens }}>
       {children}
     </NeighborhoodContext.Provider>
   );

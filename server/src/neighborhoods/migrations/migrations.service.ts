4665b386abc54c3889e10d3bbdeb9baa0be3c4bd
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2024-04-03 11:35:31 -0400

    fix: clear error when forcing claim

--- server/src/neighborhoods/migrations/migrations.service.ts
+++ server/src/neighborhoods/migrations/migrations.service.ts
@@ -154,7 +154,7 @@ export class MigrationsService {
       }
 
       // Update the locked row
-      await queryRunner.manager.update(Migration, { uuid: uuid }, { status: 2 }); // Claimed
+      await queryRunner.manager.update(Migration, { uuid: uuid }, { status: 2, error: undefined }); // Claimed
 
       // Commit the transaction, release the lock
       await queryRunner.commitTransaction();

3e2600a76f096a055349a6dde0f3808272a886c0
Justin Griffin <jgriffin555@comcast.net>
2024-11-20 18:07:02 -0600

    feat: add interval to systemwide service module to reduce query load
    Add check for 10 min interval to prevent heavy query load on counts, matching overall metrics system interval.
    Remove systemwide controller, deprecated in favor of main metrics endpoints.
    Remove unused functions from systemwide queries

--- server/src/metrics/systemwide/systemwide.module.ts
+++ server/src/metrics/systemwide/systemwide.module.ts
@@ -6,7 +6,6 @@ import { SystemWideMetric } from "../../database/entities/systemwide-metric.enti
 import { Block } from "../../database/entities/block.entity";
 import { Transaction } from "../../database/entities/transaction.entity";
 import { SystemWideService } from "./systemwide.service";
-import { SystemWideController } from "./systemwide.controller";
 import { PrometheusQueryService } from "../prometheus-query/query.service";
 import { MetricsService } from "../metrics.service";
 import { MetricsSchedulerConfigService } from "src/config/metrics-scheduler/configuration.service";
@@ -25,7 +24,6 @@ import { PrometheusQuery } from "../../database/entities/prometheus-query.entity
     PrometheusQueryService,
     ConfigService,
   ],
-  controllers: [SystemWideController],
   exports: [SystemWideService],
 })
 export class SystemWideModule {}

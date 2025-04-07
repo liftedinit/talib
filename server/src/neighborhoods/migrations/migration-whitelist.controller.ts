e301e3c310f167f358c947bb46502c44029e06b0
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-04-07 10:41:41 -0400

    fix: bech32 check

--- server/src/neighborhoods/migrations/migration-whitelist.controller.ts
+++ server/src/neighborhoods/migrations/migration-whitelist.controller.ts
@@ -31,6 +31,13 @@ export class MigrationWhitelistController {
     description: "The address was successfully added to the whitelist"
   })
   async addToWhitelist(@Body() dto: MigrationWhitelistDto): Promise<MigrationWhitelistDto> {
+    const bech32Pattern = /^manifest1[qpzry9x8gf2tvdw0s3jn54khce6mua7l]{1,58}$/i;
+
+    if (!bech32Pattern.test(dto.manifestAddress)) {
+      this.logger.debug(`Invalid bech32 address format: ${dto.manifestAddress}`);
+      throw new Error("Invalid bech32 address format");
+    }
+
     return this.whitelistService.addAddress(dto.manifestAddress);
   }
 }
